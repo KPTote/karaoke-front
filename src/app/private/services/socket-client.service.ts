9ce4b7ce928ff84ae4906f6e4c35d82b2341e2c1
Kevyn Posadas <tote.kev@gmail.com>
2025-12-01 12:36:42 -0600

    update socket client

--- src/app/private/services/socket-client.service.ts
+++ src/app/private/services/socket-client.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { WSocketRes } from '../interfaces/private.interface';
 
 @Injectable({
@@ -12,7 +13,9 @@ export class SocketClientService {
 
   public connectToWebSockets() {
 
-    const socket = new WebSocket('ws://localhost:3000/ws');
+    const baseUrl = environment.apiUrl;
+
+    const socket = new WebSocket(`ws://${baseUrl}/url`);
 
     socket.onmessage = (event) => {
       this.messageSubject.next(JSON.parse(event.data));
